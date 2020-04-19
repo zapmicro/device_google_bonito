57d07320a6acae23a4fcdec61162f26a0f217250
Sultan Alsawaf <sultan@kerneltoast.com>
2020-04-18 23:20:50 -0700

    b4s4: Force triple frame buffers
    This yields significantly reduced jitter and doesn't have any measurable
    impact on latency.
    
    Also improved jitter when testing with jankbench at slight bad frame
    cost.
    Change-Id: I1341ca855fd2635aaae9d3a4e25acdf5a7b9d665
    [kdrag0n: Changed to PRODUCT_PRODUCT_PROPERTIES and moved to device.mk
              for vendorless AOSP builds]
    
    Change-Id: I0f3d9ebdebc423db2b39b0d1f767b6a8b6a3c5d8

--- device.mk
+++ device.mk
@@ -845,14 +845,18 @@ KMGK_USE_QTI_SERVICE := true
 TARGET_CHIPSET := ""
 
 # Early phase offset configuration for SurfaceFlinger
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.early_phase_offset_ns=1500000
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.early_app_phase_offset_ns=1500000
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.early_gl_phase_offset_ns=3000000
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.early_gl_app_phase_offset_ns=15000000
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    debug.sf.early_phase_offset_ns=1500000
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    debug.sf.early_app_phase_offset_ns=1500000
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    debug.sf.early_gl_phase_offset_ns=3000000
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    debug.sf.early_gl_app_phase_offset_ns=15000000
+
+# Force triple frame buffers
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 
 # Enable backpressure for GL comp
 PRODUCT_PROPERTY_OVERRIDES += \
