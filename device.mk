355911205b61f5253e8bac36dde46cf9a169fcef
Ethan Chen <intervigil@gmail.com>
2020-09-15 16:33:57 -0400

    Add AOSP RCS packages
    Change-Id: Ic16d3b92820d246db73ad70ccaa0f4bc7f50e515

--- device.mk
+++ device.mk
@@ -914,3 +914,9 @@ PRODUCT_PACKAGES += \
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
+# RCS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    PresencePolling \
+    RcsService
